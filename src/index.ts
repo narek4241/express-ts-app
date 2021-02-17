c87853d6fbad3e1f26a87712afecf86dd3eb1155
narek4241 <narekghazaryan02@gmail.com>
2021-02-17 14:27:29 +0400

    setup express app, +3pkg(with @types)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>Hello there</div>
+  `);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
