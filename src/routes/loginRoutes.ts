9c77d84e06daab72c2f5eed2b08f8f74d1692b96
narek4241 <narekghazaryan02@gmail.com>
2021-02-18 00:10:43 +0400

    added requireAuth created middleware, protected route

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
@@ -6,11 +6,27 @@ interface RequestWithBody extends Request {
 
 const router = Router();
 
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session && req.session.isLoggedIn) {
+    next();
+    return;
+  } else {
+    res.send(`
+      <div>
+        <h1>Access Denied</h1>
+        <p>(You're logged out.)</p>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+}
+
 router.get('/', (req: Request, res: Response) => {
   if (req.session && req.session.isLoggedIn) {
     res.send(`
       <div>
         <h1>You're logged in.</h1>
+        <a href="/protected">Protected Data</a>
         <a href="/logout">Logout</a>
       </div>
     `);
@@ -67,8 +83,6 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
         </div>
       `);
   }
-
-  res.send('test');
 });
 
 router.get('/logout', (req: Request, res: Response) => {
@@ -78,6 +92,16 @@ router.get('/logout', (req: Request, res: Response) => {
   } else {
     res.redirect('/');
   }
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send(`
+      <div>
+        <h2>Welcome to protected route</h2>
+        <p>(You're logged in user)</p>
+        <a href="/">Back to Home</a>
+      </div>
+    `);
 });
 
 export { router };
